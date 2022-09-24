1a89de2e8ccccca988b94c61a8b4d2ecb4603610
jakobwamani <nicobwan@gmail.com>
2022-09-24 12:26:30 +0300

    adding_the_status_model

--- cash_app/checks/views.py
+++ cash_app/checks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from checks.models import *
 from checks.forms import *
+import snoop
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 
@@ -62,13 +63,13 @@ def delete_schools(request):
     # return HttpResponseRedirect('/view_raw_material_names/')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-def get_unchecked_slips(request):
+def define_status(request):
     context = {}
     # add the dictionary during initialization
     
     if request.method == 'POST':
 
-        form = unchecked_form(request.POST ,request.FILES) 
+        form = status_form(request.POST ,request.FILES) 
 
         if form.is_valid():
       
@@ -76,21 +77,22 @@ def get_unchecked_slips(request):
 
             return HttpResponseRedirect('/') 
     else:
-        form = unchecked_form()
-    return render(request, "unchecked.html",{'form':form})
+        form = status_form()
 
-def view_unchecked_slips(request):
-    un_checked_slips = un_checked.objects.all()
+    return render(request, "status.html",{'form':form})
+
+def view_status(request):
+    modes = status.objects.all()
      
     # return render(request, "view_supply.html", context)
-    return render(request, "view_unchecked.html", {'un_checked_slips':un_checked_slips})
+    return render(request, "view_status.html", {'modes':modes})
 
-def update_unchecked_slips(request):
+def update_status(request):
     context_dict = {}
     if 'id' in request.GET:
         pk = request.GET['id']
-        slip_record = un_checked.objects.get(id=pk)
-        form = unchecked_form(request.POST or None, instance=slip_record)
+        status_record = status.objects.get(id=pk)
+        form = status_form(request.POST or None, instance=status_record)
         if request.method == 'POST':
             if form.is_valid():           
                 form.save()
@@ -98,15 +100,69 @@ def update_unchecked_slips(request):
         else:
             context_dict["form"] = form 
 
-    return render(request,"update_unchecked.html",context=context_dict)
-    
-def delete_unchecked_slips(request):
+    return render(request,"update_status.html",context=context_dict)
+
+def delete_status(request):
     context_dict = {}
     if 'id' in request.GET:
         pk = request.GET['id']
         clean_pk = pk.strip("/")
         cleaned_pk = int(clean_pk)
-        slip_to_delete = un_checked.objects.get(id=cleaned_pk) 
+        status_to_delete = status.objects.get(id=cleaned_pk) 
+        #But before we delete , we must reduce on the amount in the RMQ model
+        #since this is an object , i will create a function right away
+        
+        status_to_delete.delete()
+    
+    # return HttpResponseRedirect('/view_raw_material_names/')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def get_slips(request):
+    context = {}
+    # add the dictionary during initialization
+    
+    if request.method == 'POST':
+
+        form = slip_form(request.POST ,request.FILES) 
+
+        if form.is_valid():
+      
+            form.save()
+
+            return HttpResponseRedirect('/') 
+    else:
+        form = slip_form()
+        
+    return render(request, "slips.html",{'form':form})
+
+def view_slips(request):
+    slips = slip.objects.all()
+     
+    # return render(request, "view_supply.html", context)
+    return render(request, "view_slips.html", {'slips':slips})
+
+def update_slips(request):
+    context_dict = {}
+    if 'id' in request.GET:
+        pk = request.GET['id']
+        slip_record = slip.objects.get(id=pk)
+        form = slip_form(request.POST or None, instance=slip_record)
+        if request.method == 'POST':
+            if form.is_valid():           
+                form.save()
+                return HttpResponseRedirect('/')   
+        else:
+            context_dict["form"] = form 
+
+    return render(request,"update_slip.html",context=context_dict)
+    
+def delete_slip(request):
+    context_dict = {}
+    if 'id' in request.GET:
+        pk = request.GET['id']
+        clean_pk = pk.strip("/")
+        cleaned_pk = int(clean_pk)
+        slip_to_delete = slip.objects.get(id=cleaned_pk) 
         #But before we delete , we must reduce on the amount in the RMQ model
         #since this is an object , i will create a function right away
         
@@ -114,3 +170,4 @@ def delete_unchecked_slips(request):
     
     # return HttpResponseRedirect('/view_raw_material_names/')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
